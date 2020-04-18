e115fe6b23ddeeb49dcda83ad3f7d8b74ff92fc0
shivamgumber <Shivammaangumber@gmail.com>
2020-04-18 18:22:36 +0530

    ok

---
+++ 17_04_2020_league_servlet_webapp/src/main/java/com/example/demo/controller/AddLeagueServlet.java
@@ -0,0 +1,63 @@
+package com.example.demo.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.example.demo.model.League;
+
+/**
+ * Servlet implementation class AddLeagueServlet
+ */
+public class AddLeagueServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddLeagueServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+		
+	}
+
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String leagueName=request.getParameter("title").toString();
+		String leagueYear=request.getParameter("year").toString();
+		String leagueSeason=request.getParameter("season").toString();
+		if((leagueName.length()<=8)&&(leagueYear.isEmpty())&& (leagueSeason.isEmpty()))
+		{
+			RequestDispatcher view=request.getRequestDispatcher("error.html");
+			view.forward(request, response);
+		}
+		else
+		{
+			RequestDispatcher view=request.getRequestDispatcher("success.html");
+			view.forward(request, response);
+		}
+		 			 
+}	   
+}
+
+
