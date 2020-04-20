b0177e324bbd6199b6b2ef8ee473a36ed3fa3c52
shivamgumber <Shivammaangumber@gmail.com>
2020-04-20 15:04:42 +0530

    okok

---
+++ 20_04_2020_spring_web_mvc/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class HelloWorldController {
+	@RequestMapping("/showForm")
+	public String showForm()
+	{
+		return "helloworld-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm(HttpServletRequest request )
+	{
+		//String str=request.getParameter("studentName");
+		return "helloworld";
+	}
+
+}
--- 20_04_2020_spring_web_mvc/src/webapp/WEB-INF/view/main-menu.jsp
+++ 20_04_2020_spring_web_mvc/src/webapp/WEB-INF/view/main-menu.jsp
@@ -8,5 +8,7 @@
 </head>
 <body>
 <h2>Spring MVC Demo - Home Page</h2>
+<hr>
+<a href="showForm">Hello World form</a>
 </body>
 </html>
