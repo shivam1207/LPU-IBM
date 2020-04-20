1d39749ea8248f59f3db62c3057aeedbafd0f1f2
shivamgumber <Shivammaangumber@gmail.com>
2020-04-20 15:28:28 +0530

    ok

--- 20_04_2020_spring_web_mvc/src/main/java/com/example/demo/controller/HelloWorldController.java
+++ 20_04_2020_spring_web_mvc/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -3,7 +3,9 @@ package com.example.demo.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloWorldController {
 	@RequestMapping("/showForm")
@@ -16,6 +18,18 @@ public class HelloWorldController {
 	public String processForm(HttpServletRequest request )
 	{
 		//String str=request.getParameter("studentName");
+		return "helloworld";
+	}
+	@RequestMapping("/processFormVersion2")
+	public String processForm(HttpServletRequest request, Model model) {
+		String message = "Yo! " + request.getParameter("studentName").toString();
+		model.addAttribute("message", message);
+		return "helloworld";
+	}
+	@RequestMapping("/processFormVersion3")
+	public String processForm(@RequestParam("studentName") String str,Model model)
+	{
+		model.addAttribute("message1",str);
 		return "helloworld";
 	}
 
