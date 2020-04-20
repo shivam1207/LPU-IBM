97ca32a6ed2eef527accb14760ce664f3eee1ae0
shivamgumber <Shivammaangumber@gmail.com>
2020-04-20 16:45:05 +0530

    okok

---
+++ 20_04_2020_spring_web_mvc/src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.model.Student;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+	
+	@RequestMapping("/showForm")
+	public String showForm(Model model)
+	{
+		
+		Student student=new Student();
+		
+		model.addAttribute("student",student);
+		return "student-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm(@ModelAttribute("student") Student theStudent,Model model)
+	{
+		theStudent.setId(UUID.randomUUID().toString());
+		model.addAttribute("student", theStudent);
+		return "student-confirmation";
+	}
+}
--- 20_04_2020_spring_web_mvc/src/webapp/WEB-INF/view/main-menu.jsp
+++ 20_04_2020_spring_web_mvc/src/webapp/WEB-INF/view/main-menu.jsp
@@ -9,6 +9,12 @@
 <body>
 <h2>Spring MVC Demo - Home Page</h2>
 <hr>
-<a href="showForm">Hello World form</a>
+<hr>
+<ul>
+
+<li><a href="hello/showForm">Hello World form</a></li>
+<li><a href="student/showForm">Student Form</a></li>
+
+</ul>
 </body>
 </html>
