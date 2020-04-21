7c9d260fc9f6550d33cabad57df268d6e9a032c9
shivamgumber <Shivammaangumber@gmail.com>
2020-04-21 12:46:55 +0530

    ok

---
+++ 21_04_2020_jackson_databind_demo/src/main/java/com/example/demo/PojoToJsonDriver.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class PojoToJsonDriver {
+	public static void main(String args[]) throws Exception {
+	Customer cust =new Customer();
+	Address address = new Address();
+	cust.setId(1);
+	cust.setFirstName("shivam");
+	cust.setAddress(address);
+	address.setCity("rudrapur");
+	
+	//Creating the ObjectMapper object
+    ObjectMapper mapper = new ObjectMapper();
+    //Converting the Object to JSONString
+    String jsonString = mapper.writeValueAsString(cust);
+    System.out.println(jsonString);
+ }
+	
+	
+
+}
