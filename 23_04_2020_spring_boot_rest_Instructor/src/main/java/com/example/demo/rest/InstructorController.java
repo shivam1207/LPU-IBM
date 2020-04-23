9776b61b117607b5c01ecf9994cebb43e5fcc7ea
shivamgumber <Shivammaangumber@gmail.com>
2020-04-23 12:07:34 +0530

    ok

--- 23_04_2020_spring_boot_rest_Instructor/src/main/java/com/example/demo/rest/InstructorController.java
+++ 23_04_2020_spring_boot_rest_Instructor/src/main/java/com/example/demo/rest/InstructorController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,7 +35,7 @@ public class InstructorController {
 		}
 		
 		@PostMapping("/instructors")
-		public Instructor createInstructor(Instructor instructor)
+		public Instructor createInstructor(@RequestBody Instructor instructor)
 		{
 			return dao.createInstructor(instructor);
 		}
